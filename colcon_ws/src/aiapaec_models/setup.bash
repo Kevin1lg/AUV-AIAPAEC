c5c01f339fda614f96b75c8605d3d76d855f58f8
Kevin Luque Gutierrez <96495440+Kevin1lg@users.noreply.github.com>
2024-04-17 11:54:35 -0500

    Delete colcon_ws/src directory

--- colcon_ws/src/aiapaec_models/setup.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Modify this for your environment
-
-# Add aeapaec_models models and worlds
-export GZ_SIM_RESOURCE_PATH=$HOME/AUV-AIAPAEC/colcon_ws/src/aiapaec_models/models:$HOME/AUV-AIAPAEC/colcon_ws/src/aiapaec_models/worlds:$GZ_SIM_RESOURCE_PATH
